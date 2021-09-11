e746a8077837ff2929a2653613c7572de45d461d
Sparsh <sparsh.m2k@gmail.com>
2021-09-11 21:47:03 +0530

    Learnt writing a shader to output a red coloured triangle
    wrote a vertex shader and fragment shader

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <malloc.h>
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+
 
 // I just have a few comments.
 
@@ -8,12 +10,54 @@
 // Make sure you have glfw3.dll and glew32.dll in root dir as well. Any questions about the building just ask. 
 
 // First I wouldn't use using namespace std if you want to know why there is a cherno video on it but it basically is for readability and there are a few other reasons.
-using namespace std;
+
+static unsigned int CompileShader( unsigned int type,std::string& Source )
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = Source.c_str();
+    glShaderSource(id, 1, &src, NULL);
+    glCompileShader(id);
+
+    int Result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &Result);
+    if(Result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)(_alloca(length * sizeof(char)));
+        glGetShaderInfoLog(id, length, &length, message);
+        
+        std::cout<<"Failed to compile"<<
+        (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") <<"shader\n";
+        std::cout<<message<<"\n";
+
+        glDeleteShader(id);
+
+        return 0;
+    }
+    return id;           
+}
+static unsigned int CreateShader(std::string& VertexShader, std::string& FragmentShader)
+{
+    unsigned int program =  glCreateProgram(); 
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, VertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, FragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    
+    return program;
+}
 
 int main()
 {
-    cout<<"A nondescript C++ program using the GLFW library, coded and built using VSCode"<<endl;
-    cout<<"Initializing Graphics Library FramWork (GLFW)"<<endl;
+    std::cout<<"A nondescript C++ program using the GLFW library, coded and built using VSCode"<<std::endl;
+    std::cout<<"Initializing Graphics Library FramWork (GLFW)"<<std::endl;
     
     // Also if you are error checking this you best out a comment telling you why it failed instead of just returning
     if(!glfwInit())
@@ -48,6 +92,30 @@ int main()
 
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::string VertexShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) in vec4 Position;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = Position;"
+    "}\n";
+
+
+    std::string FragmentShader = 
+    "#version 330 core\n"
+    "\n"
+    "layout(location = 0) out vec4 Color;"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   Color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "}\n";
+
+    unsigned int Shader = CreateShader(VertexShader, FragmentShader);
+    glUseProgram(Shader);
 
     // Application (Game) loop
     while(!glfwWindowShouldClose(gameWindow))
